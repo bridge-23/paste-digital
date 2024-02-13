6b527eb29e8d86f48fd3f082d43750b0e2ca77ff
Galiusbro <ayaalsantaev@gmail.com>
2024-02-13 23:16:19 +0800

    litle bug v2 with style fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,6 @@ export default function NewPage() {
     <div>
       {/* <HoodMap /> */}
       <FileUpload />
-      <button type="button" className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 shadow-lg shadow-cyan-500/50 dark:shadow-lg dark:shadow-cyan-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Cyan</button>
     </div>
   );
 }
